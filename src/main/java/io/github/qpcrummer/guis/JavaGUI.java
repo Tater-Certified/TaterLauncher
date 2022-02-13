ce5bf9a3a4d19810b6ac5680caea5fbd8675ba04
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2022-02-12 22:10:49 -0500

    Use tabs
    - Lot less code and cleaner

--- src/main/java/io/github/qpcrummer/guis/JavaGUI.java
+++ src/main/java/io/github/qpcrummer/guis/JavaGUI.java
@@ -11,12 +11,14 @@ public class JavaGUI {
     //Frames
     public static final JFrame javaframe = new JFrame();
     //Panels
-    public static final JPanel master = new JPanel();
     public static final JPanel profiles = new JPanel();
     public static final JPanel newpro = new JPanel();
     public static final JPanel path = new JPanel();
     public static final JPanel ram = new JPanel();
     public static final JPanel flags = new JPanel();
+    //Tabs
+    public static final JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+
     //Buttons
     public static final JButton newprofile = new JButton("Create Profile");
     //File Explorers
@@ -35,20 +37,13 @@ public class JavaGUI {
     public static final JLabel gcthreadlabel = new JLabel("Set number of GC threads");
     public static final JLabel otherargslabel = new JLabel("Add other arguments");
     public static final JLabel newprolabel = new JLabel("Create A New Profile");
-    //Scroll Panes
-    public static final JScrollPane scroll = new JScrollPane(master);
-    //Separators
-    public static final JSeparator divider1 = new JSeparator();
-    public static final JSeparator divider2 = new JSeparator();
-    public static final JSeparator divider3 = new JSeparator();
-    public static final JSeparator divider4 = new JSeparator();
     //Text Boxes
     public static final JTextField minjava = new JTextField("Use M for megabytes and G for gigabytes");
     public static final JTextField maxjava = new JTextField("Use M for megabytes and G for gigabytes");
     public static final JTextField largepagesize = new JTextField("Use M for megabytes");
     public static final JTextField gcthreads = new JTextField("If set to 0, it will use as many as needed");
     public static final JTextField otherargs = new JTextField();
-    public static final JTextField newproname = new JTextField();
+    public static final JTextField newproname = new JTextField("Profile Name");
     //CheckBoxes
     public static final JCheckBox noexplicitcheck = new JCheckBox("DisableExplicitGC - Disables this GC to stop slowdowns");
     public static final JCheckBox largepagescheck = new JCheckBox("UseLargePages");
@@ -59,26 +54,20 @@ public class JavaGUI {
 
     public static void initializejavaui() {
         //Java Frame
-        javaframe.setLayout(new BorderLayout());
+        javaframe.getContentPane().setLayout(new GridLayout(1, 1));
         javaframe.setTitle("Java Settings");
         javaframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         javaframe.pack();
         javaframe.setMinimumSize(new Dimension(555, 500));
         javaframe.setIconImage(icon);
+        //Add Tabs
+        tabbedPane.addTab("Select Profile", profiles);
+        tabbedPane.addTab("New Profile", newpro);
+        tabbedPane.addTab("Select Java", path);
+        tabbedPane.addTab("Set Ram", ram);
+        tabbedPane.addTab("Select Flags", flags);
 
-        //Add components
-        master.setLayout(new BoxLayout(master, BoxLayout.PAGE_AXIS));
-        master.add(profiles);
-        master.add(divider4);
-        master.add(newpro);
-        master.add(divider1);
-        master.add(path);
-        master.add(divider2);
-        master.add(ram);
-        master.add(divider3);
-        master.add(flags);
-        master.add(Box.createVerticalGlue());
-        javaframe.getContentPane().add(scroll);
+        javaframe.getContentPane().add(tabbedPane);
 
         //Profiles
         profiles.setBorder(BorderFactory.createEmptyBorder(10, 10, 30, 10));
@@ -141,9 +130,6 @@ public class JavaGUI {
         largepage.setFont(new Font("Serif", Font.BOLD, 12));
         otherargslabel.setFont(new Font("Serif", Font.BOLD, 18));
         newprolabel.setFont(new Font("Serif", Font.HANGING_BASELINE, 20));
-
-        //Scroll config
-        scroll.getVerticalScrollBar().setUnitIncrement(16);
 
         javaframe.addWindowListener(new WindowAdapter() {
             @Override
